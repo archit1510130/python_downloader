aa84b20419c39b6adf34e84ac6e3f2c17a53c964
System Administrator <root@archits-Air.mshome.net>
2017-08-18 23:45:28 +0530

    add file functionality

--- file_downloader.py
+++ file_downloader.py
@@ -9,8 +9,9 @@ Created on Thu Aug 17 18:17:49 2017
 from selenium import webdriver
 import requests
 from bs4 import BeautifulSoup
-
-
+from tqdm import tqdm
+chunk_size=512
+import argparse
 
 def scrape_links(html):
     # function to scarpe links from html response
@@ -26,7 +27,6 @@ def scrape_links(html):
 
 def get_google_links(limit,params):
     # function to fetch links equal to the limit
-    links=[]
     for start_index in range(0,limit,10):
         params['start']=start_index
         print(start_index)
@@ -35,7 +35,6 @@ def get_google_links(limit,params):
         page_links=scrape_links(resp.content)
         return page_links
         
-
                  
 def search(user_input):
     search_query=user_input
@@ -44,23 +43,48 @@ def search(user_input):
             }
     all_link=get_google_links(10,params)
     return all_link
-    
-def download():
-    user_input=input()
+
+def download(**args):
+    count=1
+    user_input=args['query']
+    user_input=user_input+args['file_type']+"free download"
     all_link=search(user_input)
     for link in all_link:
+        if count==4:
+            break
         book_name=link.split('/')[-1]
         print(book_name)
-        with open(book_name,'wb') as book:
-            r=requests.get(link,stream=True)
-            for block in r.iter_content(512):
-                if not block:
-                    break
-                book.write(block)
-                
-        break
+        resp=requests.get(link,stream=True)
+        if not resp.status_code==200:
+            return
+        try:
+            total_size=int(resp.headers['content-length'])
+        except KeyError:
+            total_size=len(resp.content)
+            
+        #print(total_size)
+        total_chunks=total_size/chunk_size
+        file_iterable = resp.iter_content(chunk_size=chunk_size)
+        tqdm_iter = tqdm(iterable = file_iterable,total=total_chunks,position=0,desc=book_name,
+            unit = 'KB')
+        with open("/Users/andha_coder/Python/python_downloader/book_name",'wb') as book:
+            for data in tqdm_iter:
+                book.write(data)  
+        count+=1         
+        
     
-download()    
+def main():
+    parser=argparse.ArgumentParser(description="A SIMPLE DOWNLODER")
+    parser.add_argument("query",type=str,default=None,help='specify the query')
+    parser.add_argument('-f',"--file_type",type=str,default="pdf",help='specify to describe file type')
+    args=parser.parse_args()
+    args_dict=vars(args)
+    download(**args_dict)
+        
+if __name__ == "__main__":
+	main()   
+
+
     
     
     
